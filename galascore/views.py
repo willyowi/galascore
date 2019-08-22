c6183fc8596264cfe6d9699e04d7a8e5a3d8b366
willyowi <wilsonowino1@gmail.com>
2019-08-22 23:04:30 +0300

    add galascore urls to gallery urls

--- galascore/views.py
+++ galascore/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+import datetime as dt
+
 
 # Create your views here.
+
+def pic_today(request):
+    date = dt.date.today()
+    # news = Article.todays_news()
+     return render(request, 'all-news/today-news.html', {"date": date,"news":news}) 
