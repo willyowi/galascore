5f20942849307802b1409dfe4b470ae390c65ab7
willyowi <wilsonowino1@gmail.com>
2019-08-22 23:05:30 +0300

    converting dates function

--- galascore/views.py
+++ galascore/views.py
@@ -9,3 +9,15 @@ def pic_today(request):
     date = dt.date.today()
     # news = Article.todays_news()
      return render(request, 'all-news/today-news.html', {"date": date,"news":news}) 
+
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day  
