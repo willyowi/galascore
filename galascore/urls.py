e4664e1ced90e19948c1e377e02a53e4bdde70dc
willyowi <wilsonowino1@gmail.com>
2019-08-22 22:58:56 +0300

    add url patern for todays pic

---
+++ galascore/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns=[
+    url(r'^$',views.pic_today,name='picToday'),
+    #url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+    #url(r'^search/', views.search_results, name='search_results'),
+    #url(r'^article/(\d+)',views.article,name ='article')
+
+]
