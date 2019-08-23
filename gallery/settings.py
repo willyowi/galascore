7a90c4a8d5c81f3ea49be81dab5ade267c9b3224
willyowi <wilsonowino1@gmail.com>
2019-08-23 10:19:18 +0300

    add navbar

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'galascore.apps.GalascoreConfig',
+    'galascore',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
