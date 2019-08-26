23fd1b73322483503c368755632a7070d104dfd9
willyowi <wilsonowino1@gmail.com>
2019-08-26 07:57:07 +0300

    add database querries

--- galascore/models.py
+++ galascore/models.py
@@ -2,3 +2,13 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length =10,blank = True)
+
+
+    # querrying the database
+    def __str__(self):
+        return self.first_name
