30d6c4303bc290014c50d2c1fbcf88c874579c90
willyowi <wilsonowino1@gmail.com>
2019-08-26 07:40:15 +0300

    add postgresql name user and password

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'gala',
+        'USER': 'moringa',
+        'PASSWORD':'sijuinigani',
     }
 }
 
