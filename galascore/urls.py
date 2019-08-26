84853a7d946b327b54771da24ac7502da8df442c
willyowi <wilsonowino1@gmail.com>
2019-08-26 07:22:47 +0300

    added a get started functionality

--- galascore/urls.py
+++ galascore/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    
 
-    url(r'^$',views.pic_today,name='picToday'),
-    #url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+    url(r'^pics/',views.pic_today,name='picToday')
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
     #url(r'^search/', views.search_results, name='search_results'),
     #url(r'^article/(\d+)',views.article,name ='article')
 
--- galascore/views.py
+++ galascore/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 
@@ -9,10 +9,11 @@ import datetime as dt
 def welcome(request):
     return render(request, 'welcome.html')
 
+
 def pic_today(request):
     date = dt.date.today()
-    news = Article.todays_news()
-    return render(request, 'all-pics/today-pics.html', {"date": date,"news":news}) 
+    #news = Article.todays_news()
+    return render(request, 'all-pics/today-pics.html', {"date": date}) 
 
 
 def convert_dates(dates):
