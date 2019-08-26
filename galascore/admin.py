add7ca8b4a95e872f85ff8fe178ce355bda3c63f
willyowi <wilsonowino1@gmail.com>
2019-08-26 09:51:58 +0300

    add updtaed imports in ADMIN

--- galascore/admin.py
+++ galascore/admin.py
@@ -4,7 +4,7 @@ from .models import Postee,Image,tags,Location,Category
 admin.site.register(Postee)
 admin.site.register(Image)
 admin.site.register(tags)
-admin.site.register(location)
+admin.site.register(Location)
 admin.site.register(Category)
 
 
